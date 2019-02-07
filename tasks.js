9ba698c4689ea496d6bee0a436182294f038c356
remotecharms <emma_crothers@hotmail.com>
2019-02-07 16:22:50 +0000

    hard coded tasks

--- tasks.js
+++ tasks.js
@@ -4,12 +4,32 @@ const app = express();
 
 app.get('/tasks', function (request, response) {
 
-  const username = request.query.username;
+  const taskList= {
+  
+      task: [ {
+        TaskId: 1,
+        Description: "Learn JavaScript",
+        Completed: false
+      },
+      {
+        TaskId: 2,
+        Description: "Learn MySQL",
+        Completed: false
+      },
+      {
+        TaskId: 3,
+        Description: "Find out if Mary had a little Lambda",
+        Completed: false
+      }
+    ]};
+  
+  /*const username = request.query.username;
 
-  const someJson = {
+  /*const taskList = {
     message: "Hello " + username
   };
-  response.json(someJson);
+  */
+  response.json(taskList);
 })
 
 module.exports.handler = serverless(app);
