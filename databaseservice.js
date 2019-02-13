7515444e1dfdb9c51b9bf88dd8cdc10c829fe30d
remotecharms <emma_crothers@hotmail.com>
2019-02-13 22:14:26 +0000

    delete function

---
+++ databaseservice.js
@@ -0,0 +1,61 @@
+const mysql = require("mysql"); 
+
+function getDatabaseConnection() {
+    return mysql.createConnection({
+        host: process.env.RDS_HOST,
+        user: process.env.RDS_USER,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DATABASE
+    });
+}
+//environment variables - you wouldn't hard code the password or the username, because anyone could connect to the database
+// with the creditentials. Used for security and allows code to flex for the enviornment e.g. Dev vs Prod etc. 
+function getTasks() {
+    const connection = getDatabaseConnection();
+    return new Promise(function(resolve, reject) {
+        connection.query("SELECT * FROM Tasks", function(error, results, fields) {
+            if (error) {
+                connection.destroy();
+                return reject(error);
+            } 
+            else {
+                connection.end();
+                return resolve(results);
+            }
+        });
+    });
+
+    function deleteTasks(taskId) {
+        const connection = getDatabaseConnection();
+        
+        return new Promise(function(resolve, reject) {
+            connection.query("DELETE FROM Tasks WHERE TaskId =?", taskId, function(error, results, fields) {
+                if (error) {
+                    connection.destroy();
+                    return reject(error);
+                } 
+                else {
+                    connection.end();
+                    return resolve(results);
+                }
+            });
+        });
+}
+}
+// the above brings in promises and MySQL 
+// call back function - it returns 3 things if there is an error, if it succeeded then it gets the results 
+// if there is an error first of all we make sure that the connection is terminated and then it returns
+// if it works in the else return the resolve and then the results
+// we need to end the connection - if the app stays up and running, when the app starts you estabilish the connection to the database
+// you tend to keep ipen the connections to the database
+// it's a TCP Handshake 
+// with Lambda the code does not exist until someone calls it everytime a query is issued to the database you need to restart the connection
+// you're waiting for Javascripts garabage collection to timeout, `If you were using a server app intialise a pool of connection 
+// it will keep open multiple connections. 
+// logs can be found on the server so that our can see termianted connections. 
+// fields is an array of fields that you are querying from the database 
+module.exports = {
+ getTasks,
+ deleteTasks
+}
+
--- tasks.js
+++ tasks.js
@@ -2,46 +2,38 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 
+const databaseService = require('./databaseservice');
+
 app.get('/tasks', function (request, response) {
 
-  const taskList= {
-  
-      task: [ {
-        TaskId: 1,
-        Description: "Learn JavaScript",
-        Completed: false
-      },
-      {
-        TaskId: 2,
-        Description: "Learn MySQL",
-        Completed: false
-      },
-      {
-        TaskId: 3,
-        Description: "Find out if Mary had a little Lambda",
-        Completed: false
-      }
-    ]};
-  
-  /*const username = request.query.username;
+  databaseService.getTasks()
+  .then(function(results) {
+    //we got the Tasks ok
+    response.json(results);
 
-  /*const taskList = {
-    message: "Hello " + username
-  };
-  */
-  response.json(taskList);
+  })
+  .catch (function (error) {
+    // soemthing went weong when getting the tasks
+    response.status(500);
+    response.json(error);
+  });
+
 })
 
 app.delete('/tasks/:taskId', function (request, response) {
 
-  const taskIdToBeDeleted = request.params.taskId;
+  const taskId = request.params.taskId
+  databaseService.deleteTasks(taskId)
+  .then(function(results) {
+    //we got the Tasks ok
+    response.json(results);
 
-  const someResponse = {
-    message: "You executed a delete request for ID: " + taskIdToBeDeleted
-  };
-
-  response.json(someResponse);
-
-});
+  })
+  .catch (function (error) {
+    // soemthing went weong when getting the tasks
+    response.status(500);
+    response.json(error);
+  });
+})
 
 module.exports.handler = serverless(app);
